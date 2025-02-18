8603956a9f5fac0f84ec04f474490443f500bec8
Pusztai Dominik <zsigulipuszz@gmail.com>
2025-02-18 12:08:51 +0100

    Implemented widget one for the counter

--- ScopedServiceApp/src/app/components/widget-one/widget-one.component.ts
+++ ScopedServiceApp/src/app/components/widget-one/widget-one.component.ts
@@ -1,14 +1,33 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { MyScopedService } from 'src/app/services/my-scoped-service.service';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-widget-one',
+  standalone: true,
+  imports: [CommonModule],
   templateUrl: './widget-one.component.html',
   styleUrls: ['./widget-one.component.scss'],
 })
-export class WidgetOneComponent  implements OnInit {
+export class WidgetOneComponent  implements OnInit, OnDestroy {
+  counterValue: number = 0;
+  private subscription!: Subscription;
 
-  constructor() { }
+  constructor(private myScopedService: MyScopedService) {}
 
-  ngOnInit() {}
+  ngOnInit() {
+    //console.log("WidgetOne feliratkozás!");
+    this.subscription = this.myScopedService.counterObservable$.subscribe(
+      (value) => {
+        this.counterValue = value;
+        //console.log("WidgetOne kapott új értéket:", value);
+      }
+    );
+  }
 
+  ngOnDestroy() {
+    console.log("WidgetOne leiratkozás!");
+    this.subscription.unsubscribe();
+  }
 }
