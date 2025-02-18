774528e142aaec3338b5ed99fa59d522b043ae64
Pusztai Dominik <zsigulipuszz@gmail.com>
2025-02-18 12:09:29 +0100

    Implemented widget two for the random number

--- ScopedServiceApp/src/app/components/widget-two/widget-two.component.ts
+++ ScopedServiceApp/src/app/components/widget-two/widget-two.component.ts
@@ -1,14 +1,28 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { MyScopedService } from '../../services/my-scoped-service.service';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-widget-two',
+  standalone: true,
+  imports: [CommonModule],
   templateUrl: './widget-two.component.html',
   styleUrls: ['./widget-two.component.scss'],
 })
-export class WidgetTwoComponent  implements OnInit {
+export class WidgetTwoComponent  implements OnInit, OnDestroy {
+  randomValue: number = 0;
+  private subscription!: Subscription;
 
-  constructor() { }
+  constructor(private myScopedService: MyScopedService) { }
 
-  ngOnInit() {}
+  ngOnInit() {
+    this.subscription = this.myScopedService.randomNumberObservable$.subscribe(
+      (value) => (this.randomValue = value)
+    );
+  }
 
+  ngOnDestroy() {
+      this.subscription.unsubscribe();
+  }
 }
