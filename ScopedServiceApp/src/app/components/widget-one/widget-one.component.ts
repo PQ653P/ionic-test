8301eb08d52bfb001f37f70d88f7615b3fb5ea71
Pusztai Dominik <zsigulipuszz@gmail.com>
2025-02-19 09:46:52 +0100

    Updated design for the widgets (some animation)

--- ScopedServiceApp/src/app/components/widget-one/widget-one.component.ts
+++ ScopedServiceApp/src/app/components/widget-one/widget-one.component.ts
@@ -2,17 +2,19 @@ import { Component, OnDestroy, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MyScopedService } from 'src/app/services/my-scoped-service.service';
 import { Subscription } from 'rxjs';
+import { IonicModule } from '@ionic/angular';
 
 @Component({
   selector: 'app-widget-one',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, IonicModule],
   templateUrl: './widget-one.component.html',
   styleUrls: ['./widget-one.component.scss'],
 })
 export class WidgetOneComponent  implements OnInit, OnDestroy {
   counterValue: number = 0;
   private subscription!: Subscription;
+  animate = false;
 
   constructor(private myScopedService: MyScopedService) {}
 
@@ -21,6 +23,7 @@ export class WidgetOneComponent  implements OnInit, OnDestroy {
     this.subscription = this.myScopedService.counterObservable$.subscribe(
       (value) => {
         this.counterValue = value;
+        this.triggerAnimation();
         //console.log("WidgetOne kapott új értéket:", value);
       }
     );
@@ -29,5 +32,10 @@ export class WidgetOneComponent  implements OnInit, OnDestroy {
   ngOnDestroy() {
     console.log("WidgetOne leiratkozás!");
     this.subscription.unsubscribe();
+  }
+
+  triggerAnimation() {
+    this.animate = true;
+    setTimeout(() => (this.animate = false), 300);
   }
 }
